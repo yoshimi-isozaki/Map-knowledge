5dd4f34c3d7047311829dafd8c49b15e9728d48d
yoshimi-isozaki <yoshimi.isozaki@gmail.com>
2021-02-25 15:24:31 +0900

    {'緯度': 33.552993, '経度': 130.307399}の形でファイルを取得できた

--- Park-knowledge/park_test02.py
+++ Park-knowledge/park_test02.py
@@ -7,37 +7,66 @@ https://www.geocoding.jp/
 """
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import re
 import time
 from bs4 import BeautifulSoup
 
 # Google Chromeのドライバを用意
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-
+wait = WebDriverWait(driver, 10)
 # geocoding.jpを開く
 # 直接小笹３号公園にクエリで入る
 
-#url = 'https://www.geocoding.jp/?q=小笹３号公園%20福岡'33.560544 経度: 130.387521
+#url = 'https://www.geocoding.jp/?q=野方８号%20公園%20福岡'33.560544 経度: 130.387521
 # 内側の検索機能にアクセスする
-url = 'https://www.geocoding.jp/?q=1'
+url = 'https://www.geocoding.jp/?q=野方８号%20公園%20福岡市%20西区'
 driver.get(url)
 
 time.sleep(3)
-
+"""
 keys = '小笹３号公園 福岡'
 
 id = driver.find_element_by_id("searchbox")
-id.send_keys(keys)
-
+id.send_keys(keys) # 検索ボックスに検索キーワードを入力
+"""
 time.sleep(1)
 
-search_button = driver.find_element_by_xpath("//*[@id='show-map-link']/a")
+# list_one = wait.until(EC.element_to_be_clickable((By.XPATH, "*[@id='pane']/div/div[1]/div/div/div[4]/div[1]/div[1]")))
+search_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='show-map-link']/a")))
 #search_button.click()
-id.send_keys(Keys.ENTER)
+#id.send_keys(Keys.ENTER)
 
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
 
 #lat_long = soup.select('#result')
-lat_long = soup.find('span', class_="nowrap")
+#lat_long = soup.find('span', class_="nowrap")
+spans = soup.find_all('span', class_ ='nowrap')
+line = None
+for elem in spans:
+    print(str(elem.text))
+    if '緯' in str(elem.text):# '緯'を含むもの。経緯度の<span>要素だけ欲しい
+        line = elem
 
+print('----------------')
+print(line.text)# [緯度: 33.560544 経度: 130.387521]
+
+lat_long = {} # latが緯度、longが経度
+list = []
+
+
+for val in line.text.split():
+    try:
+        val = float(val)
+    except ValueError:
+        val = None
+    else:
+        list.append(val)
+
+print(list) #[33.560544, 130.387521]となる
+lat_long['緯度'] = list[0]
+lat_long['経度'] = list[1]
 print(lat_long)
--- Park-knowledge/test00.py
+++ Park-knowledge/test00.py
@@ -1,7 +1,27 @@
-import re
-match = re.search(r'@*/', 'https://www.google.co.jp/maps/place/%E9%A6%99%E6%A4%8E%E9%A7%85%E6%9D%B1%E5%85%AC%E5%9C%92/@33.6565064,130.4430107,15z/data=!4m8!1m2!2m1!1z56aP5bKh5biCIOmmmeakjuOAgOWFrOWckg!3m4!1s0x35418f3c67d64d13:0x8fb637a525115409!8m2!3d33.6582798!4d130.4445994')
-url = 'https://www.google.co.jp/maps/place/%E9%A6%99%E6%A4%8E%E9%A7%85%E6%9D%B1%E5%85%AC%E5%9C%92/@33.6565064,130.4430107,15z/data=!4m8!1m2!2m1!1z56aP5bKh5biCIOmmmeakjuOAgOWFrOWckg!3m4!1s0x35418f3c67d64d13:0x8fb637a525115409!8m2!3d33.6582798!4d130.4445994'
-print('password:', match.group())
-print(url.find('@')) # 91
-print(url.find('z/')) # 117
-print(url[91+1:117-3])
+
+sample_text = '緯度: 33.560544 経度: 130.387521'
+
+lat_long = {} # latが緯度、longが経度
+list = []
+
+"""
+for tmp in sample_text.split():
+    if type(tmp) == "<class 'float'>":
+        list.append(tmp)
+print(list)
+"""
+
+for val in sample_text.split():
+    try:
+        val = float(val)
+    except ValueError:
+        val = None
+    else:
+        list.append(val)
+
+print(list) #[33.560544, 130.387521]となる
+lat_long['緯度'] = list[0]
+lat_long['経度'] = list[1]
+print(lat_long)
+
+
